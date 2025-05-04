287fb8dcfb1aaee09862cb87561c441f39c9bd75
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-05-04 19:48:17 +0300

    Add Statuses app and tests for it

---
+++ task_manager/statuses/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Status(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name=_('Name'),
+        unique=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

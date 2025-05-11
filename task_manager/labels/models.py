c0ed583748c125eea0c9dcf53b2e7adec873e56b
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-05-11 22:32:03 +0300

    Prevent deletion of labels that are used in tasks

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.db.models.deletion import ProtectedError
 
 
 class Label(models.Model):
@@ -14,6 +15,14 @@ class Label(models.Model):
         }
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        if self.task_set.exists():
+            raise ProtectedError(
+                _("Cannot delete this label because they are being used"),
+                self
+            )
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.name

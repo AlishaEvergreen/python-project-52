e4cff0ad3934ed58dafdc759a2175ca3c9086e1b
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-05-19 19:44:46 +0300

    Create static folder, add favicon.ico and link in base template

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -155,6 +155,9 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
